180f2101376479f5f9ec2a8b346429b00bf3f8bf
Frank <southboske@gmail.com>
2016-07-17 18:25:45 -0500

    Initial Commit

---
+++ BlueMoonLight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlueMoonLight
+//
+//  Created by Frank Tellez on 7/17/16.
+//  Copyright © 2016 Frank Tellez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
